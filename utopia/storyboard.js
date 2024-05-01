c24d46f4350baaef9209b6c4745d1d64f68cad91
Liad Yosef <liad.yosef@gmail.com>
2024-05-01 14:30:15 +0300

    add favorites

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -1,39 +1,20 @@
-import * as React from 'react'
-import { Scene, Storyboard, RemixScene } from 'utopia-api'
-import { App } from '/src/app.js'
-import { Playground } from '/src/playground.js'
-import Index from '/src/routes/_index'
-import { Group } from 'utopia-api'
-import { MoodBoard } from '/src/mood-board.js'
-import { Title } from '/src/title'
+import * as React from "react";
+import { Storyboard, RemixScene } from "utopia-api";
 
 export var storyboard = (
   <Storyboard>
     <RemixScene
-      className='my-class'
+      className="my-class"
       style={{
-        position: 'absolute',
+        position: "absolute",
         width: 834,
         height: 1328,
         left: 8,
         top: -24,
-        overflow: 'hidden',
+        overflow: "hidden",
       }}
-      data-label='Mood Board'
-      commentId='1bd'
-    />
-    <RemixScene
-      className='my-class'
-      style={{
-        position: 'absolute',
-        width: 834,
-        height: 1328,
-        left: 916,
-        top: -24,
-        overflow: 'hidden',
-      }}
-      data-label='Mood Board'
-      commentId='5b7'
+      data-label="Mood Board"
+      commentId="1bd"
     />
   </Storyboard>
-)
+);
